fd0ef04a2ebfed5272eac11a4a677be276b9b8ed
jmacj <jm.nz0723@gmail.com>
2018-07-05 07:54:37 +0800

    integrated deposit/withdraw

--- application/config/routes.php
+++ application/config/routes.php
@@ -60,6 +60,8 @@ $route['transact/transfer']['GET'] = "WebsiteController/tellerView/transferFunds
 $route['transact/transfer']['POST'] = "TransactionController/otc_fund_transfer";
 $route['transact/otc/deposit']['GET'] = "WebsiteController/tellerView/depositAccount";
 $route['transact/otc/deposit']['POST'] = "TransactionController/otc_deposit";
+$route['transact/otc/withdraw']['GET'] = "WebsiteController/tellerView/withdrawAccount";
+$route['transact/otc/withdraw']['POST'] = "TransactionController/otc_withdrawal";
 /*
  * Account routes
  */
